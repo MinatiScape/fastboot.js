05621a74ba1cbb8e0f8f7658240e72f06ca664aa
Aryan Sinha <sinha.aryan03@gmail.com>
2025-03-13 15:13:33 +0530

    test

--- src/factory.ts
+++ src/factory.ts
@@ -238,12 +238,6 @@ export async function flashZip(
         tryReboot(device, "bootloader", onReconnect)
     );
 
-    // Cancel snapshot update if in progress
-    let snapshotStatus = await device.getVariable("snapshot-update-status");
-    if (snapshotStatus !== null && snapshotStatus !== "none") {
-        await device.runCommand("snapshot-update:cancel");
-    }
-
     // Load nested images for the following steps
     common.logDebug("Loading nested images from zip");
     onProgress("unpack", "images", 0.0);
